62f38df573d0cf809f67afd9d71470fd0a046e95
Ravikiran <ravikiran@mudah.my>
2022-02-10 16:52:52 +0800

    added new schemna

---
+++ ConnectFour/ConnectFourTests/ConnectFourStagingTests.swift
@@ -0,0 +1,33 @@
+//
+//  ConnectFourStagingTests.swift
+//  ConnectFourTests
+//
+//  Created by Gajula Ravi Kiran on 10/02/2022.
+//
+
+import XCTest
+
+class ConnectFourStagingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testStagingMethodInBitRise() throws {
+        let name = "testing"
+        XCTAssertEqual(name, "testing")
+    }
+
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
